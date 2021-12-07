b5e1aaa9193112197574be7b4efb7ec7cbb143e9
Edgardo González <edgardogonzp@gmail.com>
2021-12-06 22:37:46 -0400

    Modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SequenceEqualOperator } from 'rxjs/internal/operators/sequenceEqual';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CameraComponent } from './camera/camera.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MainComponent } from './main/main.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+/* Components */
+import { TopBarComponent } from "./top-bar/top-bar.component";
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'camera', component: CameraComponent},
+  {path: 'calculator', component: CalculatorComponent},  
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'settings', component: SettingsComponent},  
+  {path: 'top-bar', component: TopBarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,61 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+/* Material */
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRippleModule} from '@angular/material/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from "@angular/material/select";
+
+/* Components */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CameraComponent } from './camera/camera.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MainComponent } from './main/main.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MainComponent,
+    BottomBarComponent,
+    CameraComponent,
+    SettingsComponent,
+    CalculatorComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    /* Material */
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatRippleModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
   ],
-  providers: [],
+  providers: [SettingsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
